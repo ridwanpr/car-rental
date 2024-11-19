2736a4e402185e3ca50fabdb126da4fc78fe0b7b
Ridwan <ridwanpr@outlook.com>
2024-11-19 09:53:32 +0700

    replace confirm-password component with bootstrap html view

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,27 +1,50 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </div>
+@extends('layouts.backend.guest')
 
-    <form method="POST" action="{{ route('password.confirm') }}">
-        @csrf
-
-        <!-- Password -->
-        <div>
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@section('content')
+    <main>
+        <section class="mt-4 d-flex align-items-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <div class="bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 mb-4 fmxw-500">
+                            <div class="text-center text-md-center mb-4 mt-md-0">
+                                <h1 class="mb-0 h3">Confirm Password</h1>
+                            </div>
+                            <div class="text-sm text-gray-600 mb-4">
+                                {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+                            </div>
+                            <form method="POST" action="{{ route('password.confirm') }}" class="mt-4">
+                                @csrf
+                                <div class="form-group mb-4">
+                                    <label for="password">{{ __('Password') }}</label>
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="basic-addon2">
+                                            <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                        </span>
+                                        <input type="password" id="password" name="password" required
+                                            autocomplete="current-password" class="form-control" placeholder="Password">
+                                    </div>
+                                    @if ($errors->has('password'))
+                                        <div class="mt-2 text-danger">
+                                            {{ $errors->first('password') }}
+                                        </div>
+                                    @endif
+                                </div>
+                                <div class="d-grid mt-4">
+                                    <button type="submit" class="btn btn-gray-800">
+                                        {{ __('Confirm') }}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
