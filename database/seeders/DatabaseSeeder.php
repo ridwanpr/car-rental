6652780e9edbc3eb4b9a214b388b061ba73b1306
Ridwan <ridwanpr@outlook.com>
2024-11-21 01:15:36 +0700

    add user db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role_id' => User::ROLE_ADMIN,
+            'email_verified_at' => now(),
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role_id' => User::ROLE_USER,
+            'email_verified_at' => now(),
         ]);
     }
 }
