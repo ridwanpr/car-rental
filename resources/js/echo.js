b047184e3254ad20ff6d7303126baa19fda5d9a4
Ridwan <ridwanpr@outlook.com>
2025-01-08 21:24:09 +0700

    revert

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -9,7 +9,6 @@ window.Echo = new Echo({
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    wsPath: import.meta.env.VITE_REVERB_PATH ?? "",
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
